f2c725916e3b2b7f4ed4b52db4ad042ec355141c
Juan Savedra dos Santos <juansavedra827@gmail.com>
2021-08-05 14:00:47 -0300

    data types: string

---
+++ data_types.js
@@ -0,0 +1,23 @@
+// String.
+
+//Diferença entre Aspas Duplas e Aspas Simples.
+/* Usamos aspas duplas quando queremos colocar uma aspas 
+simples dentro do texto de forma que não irá atrapalhar o 
+código e vice-versa*/
+
+//Exemplo:
+
+console.log("Meu nome é 'Juan'");
+console.log('Meu nome é "Juan"');
+
+//Crases.
+
+/* Permite usar Aspas Dupĺas e Simples quando quiser dentro
+delas, além de permitir expressões (interpolação) e 
+multi-linhas. */
+
+//Exemplo:
+
+console.log(`Permite eu usar "Aspas Duplas" e "Aspas 
+Simples, além de cortar as linhas e usar expressões como
+1 + 1 = ${1 + 1}"`);
