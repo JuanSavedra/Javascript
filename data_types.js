9937f3b3d6dc89d5089a451c0eb4de83358116f5
Juan Savedra dos Santos <juansavedra827@gmail.com>
2021-08-05 14:11:10 -0300

    data types: number

--- data_types.js
+++ data_types.js
@@ -1,6 +1,7 @@
 // String.
 
 //Diferença entre Aspas Duplas e Aspas Simples.
+
 /* Usamos aspas duplas quando queremos colocar uma aspas 
 simples dentro do texto de forma que não irá atrapalhar o 
 código e vice-versa*/
@@ -21,3 +22,29 @@ multi-linhas. */
 console.log(`Permite eu usar "Aspas Duplas" e "Aspas 
 Simples, além de cortar as linhas e usar expressões como
 1 + 1 = ${1 + 1}"`);
+
+//Number.
+
+/* Permite usar números reais, inteiros, calcular expressões
+dentro dela, usar não números (NaN) e infinito (Infinity) */
+
+//Exemplo:
+
+//Número Inteiro (int).
+console.log(12);
+
+//Números Reais (float).
+console.log(12.5);
+
+//Cálculos.
+console.log((15 + 5) / 2);
+
+//Não Número (NaN).
+console.log(25 / "Juan");
+
+//Infinito (Infinity).
+console.log(12 == Infinity);
+console.log(Infinity == Infinity);
+console.log(Infinity);
+
+
