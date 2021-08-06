e79a839f22a9eca55d1d76a7d1cc66ae7e51b566
Juan Savedra dos Santos <juansavedra827@gmail.com>
2021-08-06 11:39:01 -0300

    concatenating variables

---
+++ concatenando_variáveis.js
@@ -0,0 +1,11 @@
+/* Concatenar uma variável é basicamente
+agrupá-la junto de algo. */
+
+//Exemplo:
+
+{
+    let nome, idade;
+    nome = "Juan";
+    idade = "15";
+    console.log("Nome: " + nome + " Idade: " + idade);
+}
